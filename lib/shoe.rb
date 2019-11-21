bceaba4e96d0acc7f4fb571093b7908c88e5bf47
Akpojotor Shemi <akpojotor@yahoo.com>
2019-11-21 22:10:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,7 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  BRANDS=[]
 
   def initialize(brand)
     @brand = brand
@@ -10,5 +11,9 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  def brand=(value)
+    return unless BRANDS.include?(value)
+    @brand=(value)
+    BRANDS << value
 
 end
